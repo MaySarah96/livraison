fc4b283a247110b6f96a04047c46063f30081edb
DESKTOP-Q8K03UB\MaySarah_Ameur <May.s.ameur@gmail.com>
2020-06-10 01:02:57 +0100

    localhost adr

--- app/src/main/java/com/tn/flow/livreurapp/util/ApiUtil.java
+++ app/src/main/java/com/tn/flow/livreurapp/util/ApiUtil.java
@@ -3,7 +3,7 @@ package com.tn.flow.livreurapp.util;
 
 public class ApiUtil {
 
-    private static final String BASE_URL = "http://localhost:8081/SpringMVC/servlet";
+    private static final String BASE_URL = " http://10.0.2.2:8081/SpringMVC/servlet";
 
     public static RetrofitInterface getServiceClass(){
         return RetrofitAPI.getRetrofit(BASE_URL).create(RetrofitInterface.class);
